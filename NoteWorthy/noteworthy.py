118f296a3e59af71a7710ac9e3e01b670acc45c6
David Parkin <david@davidparkin.com>
2019-03-19 09:11:53 +0000

    mouse clicks in noteworthy.py after add(canvas)

--- NoteWorthy/noteworthy.py
+++ NoteWorthy/noteworthy.py
@@ -9,6 +9,7 @@ from numpy import arange, sin, pi
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_gtk3agg import FigureCanvasGTK3Agg as FigureCanvas
+import matplotlib.patches as mpatches
 
 # a Gtk ApplicationWindow
 
@@ -24,14 +25,27 @@ class MyWindow(Gtk.ApplicationWindow):
         audio, sr = librosa.load(audio_file, sr=44100, mono=True, offset=5.0, duration=5.0)
         return audio, sr
 
+    def onclick(self, event):
+        print('%s click: button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %
+            ('double' if event.dblclick else 'single', event.button,
+            event.x, event.y, event.xdata, event.ydata))
+
     def __init__(self, app):
         Gtk.Window.__init__(self, title="Welcome to GNOME", application=app)
         y, sr = self.get_audio('filename')
-        # plt.figure()
-        # plt.subplot(3, 1, 1)
         self.set_default_size(600, 400)
         f = Figure(figsize=(5, 5), dpi=100)
         # ax = mplfigure.add_subplot(311)
+        canvas = FigureCanvas(f)
+        self.add(canvas)
+        def onclick(event):
+            print('%s click: button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %
+                ('double' if event.dblclick else 'single', event.button,
+                event.x, event.y, event.xdata, event.ydata))
+
+        cid = canvas.mpl_connect('button_press_event', onclick)
+        cid = f.callbacks.connect('button_press_event', self.onclick)
+        print(cid)
         ax1 = f.add_subplot(212)
         ax1.margin = (2, 2)
         ax1.set_title('One')
@@ -41,10 +55,9 @@ class MyWindow(Gtk.ApplicationWindow):
         ax2 = f.add_subplot(211)
         ax2.margin = (2, 2)
         ax2.set_title('Two')
+        ax2.set_facecolor('red')
+        # see https://librosa.github.io/librosa/generated/librosa.display.waveplot.html#librosa.display.waveplot
         librosa.display.waveplot(y, sr=sr, ax=ax2)
-        import ipdb; ipdb.set_trace()
-        # ax2.plot()
-        # ax2.add_collection(librosa.display.waveplot(y, sr=sr))
 
         sw = Gtk.ScrolledWindow()
         import ipdb; ipdb.set_trace()
@@ -55,9 +68,9 @@ class MyWindow(Gtk.ApplicationWindow):
         # f = Figure(figsize=(5, 5), dpi=100)
         canvas = FigureCanvas(f)  # a gtk.DrawingArea
         sw.add_with_viewport(canvas)
-        # plt.title('Monophonic')
-        # plt.tight_layout()
-        # plt.show()
+        c = mpatches.Rectangle((0.5, 0.5), 1, 1, facecolor="green",
+                    edgecolor="red", linewidth=3, alpha=0.5)
+        ax2.add_patch(c)
 
 
 class MyApplication(Gtk.Application):
